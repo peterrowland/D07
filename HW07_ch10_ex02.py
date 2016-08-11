ae7eadd0f033eb3504402e0783d56b6349759a33
peter <peter@peters-Mac-Pro.local>
2016-08-10 22:41:40 -0700

    recursion working, printing all items

--- HW07_ch10_ex02.py
+++ HW07_ch10_ex02.py
@@ -1,3 +1,7 @@
+
+#!/usr/bin/env python3
+# <filename>
+
 # I want to be able to call capitalize_nested from main w/ various lists
 # and get returned a new nested list with all strings capitalized.
 # Ex. ['apple', ['bear'], 'cat']
@@ -5,3 +9,40 @@
 # In your final submission:
 #  - Do not print anything extraneous!
 #  - Do not put anything but pass in main()
+
+##############################################################################
+# Imports
+
+
+# Body
+def capitalize_nested(list):
+
+    # list loop
+    if type(list) == type([]):
+        for item in list:
+            if type(item) == type([]):
+                capitalize_nested(item)
+                # test that it reads
+            else:
+                capitalized = item.capitalize()
+                print(capitalized)
+                # return capitalized
+
+
+# if list: enter innerloop on sublist
+    # run capitalize on sub list
+# if string: run capitalize on list[n]
+
+# return capitalized list
+
+##############################################################################
+def main():
+
+    # capitalize_nested(['a','b','c',['a','b']])
+    l = capitalize_nested(['apple', ['bear','sloth'], 'cat'])
+    print(l)
+    #TODO: try calling and printing loop down here first
+
+
+if __name__ == '__main__':
+    main()
